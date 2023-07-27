c9252492e664892635d00dadcee020c6a01deadf
hesrondrake89 <113859517+hesrondrake89@users.noreply.github.com>
2023-07-27 15:13:08 +0000

    tasks

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
+'''
+A package designed to handle Pascal's triangle operations.
+'''
+
 
 def pascal_triangle(n):
-    """
-  Returns a list of lists of integers representing the Pascal's triangle of n.
-
-  Args:
-    n: The number of rows in the triangle.
-
-  Returns:
-    A list of lists of integers.
-  """
-    res = []
-    if n > 0:
-        for i in range(1, n + 1):
-            level = []
-            C = 1
-            for j in range(1, i + 1):
-                level.append(C)
-                C = C * (i - j) // j
-            res.append(level)
-    return res
+    '''Generates a nested list of integers that represents Pascal's triangle for a given integer.
+    '''
+    triangle = []
+    if type(n) is not int or n <= 0:
+        return triangle
+    for i in range(n):
+        line = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                line.append(1)
+            elif i > 0 and j > 0:
+                line.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        triangle.append(line)
+    return triangle

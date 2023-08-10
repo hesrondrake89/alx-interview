a826210bfaa0869c3d2e40b922e67545f3ce9153
hesrondrake89 <113859517+hesrondrake89@users.noreply.github.com>
2023-08-10 16:44:02 +0000

    tasks

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,6 +4,7 @@ This script calculates the minimum number of operations required to obtain
 exactly n occurrences of the character 'H' in a file.
 """
 
+
 def minOperations(n):
     operation_count = 0
     number = 2
